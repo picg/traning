9c200027c4a2742313665c0e48e331e693226a77
kazushi eguchi <eguchi@wons.co.jp>
2016-10-19 18:17:03 +0800

    test

---
+++ test1/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- confug: utf-8 -*-
+{
+    'name': 'Test1',
+    'version': '10.0.1.0.0',
+    'description': """
+    """,
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
